5a63ad39de61f64bcd61996b060947d20378b4b8
Vogel <vogel@github.com>
2018-09-11 10:39:54 -0300

    .

---
+++ src/Program/Program.cs
@@ -0,0 +1,19 @@
+﻿//Autor: @guillevogel11
+
+using System;
+using Monitor;
+using Reporter;
+using Temp;
+
+namespace Program {
+    class Program {
+        static void Main (string[] args) 
+        {
+            TemperatureReporter reporter1 = new TemperatureReporter ();
+            TemperatureMonitor monitor1 = new TemperatureMonitor ();
+
+            reporter1.Subscribe(monitor1);
+            monitor1.GetTemperature();
+        }
+    }
+}
